e2850482b9c327e7c0c1cee3262dee6a5be0f337
jaqueline <jaqueline.abreu@fliper.app>
2022-02-08 14:13:30 -0300

    Meus primeiros arquivos de teste

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fliperdev/Teste
+
+go 1.17
---
+++ ola.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Ola() string {
+	return "Olá, mundo"
+}
+
+//regra de negócio
+func main() {
+	//fmt.Println("Olá, mundo")
+	fmt.Println(Ola())
+}
