36a427f31d003fdafd7008ec98cf8a090759a4b5
jaqueline <jaqueline.abreu@fliper.app>
2022-02-09 09:13:31 -0300

    Teste que pode causar problemas

--- ola_test.go
+++ ola_test.go
@@ -3,16 +3,27 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	// resultado := Ola()
-	// esperado := "Olá, mundo"
 
-	resultado := Ola("Chris")
-	esperado := "Olá, Chris"
-
-	//Teste que que não deve passar
-
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris")
+		esperado := "Olá, Chris"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+
+	})
+
+	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
 
 }
