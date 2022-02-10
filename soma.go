7f23512bcfc15e65a9e69718bbaeca89e1f2def4
jaqueline <jaqueline.abreu@fliper.app>
2022-02-10 14:27:43 -0300

    for com range

--- soma.go
+++ soma.go
@@ -1,9 +1,17 @@
 package main
 
+// func Soma(numeros [5]int) int {
+// 	soma := 0
+// 	for i := 0; i < 5; i++ {
+// 		soma += numeros[i]
+// 	}
+// 	return soma
+// }
+
 func Soma(numeros [5]int) int {
 	soma := 0
-	for i := 0; i < 5; i++ {
-		soma += numeros[i]
+	for _, numero := range numeros {
+		soma += numero
 	}
 	return soma
 }
