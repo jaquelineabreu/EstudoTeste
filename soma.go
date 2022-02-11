6f13f92a10029a1e846aa5d7d13da64978e1b4d0
jaqueline <jaqueline.abreu@fliper.app>
2022-02-10 23:22:47 -0300

    Teste concluido com slices

--- soma.go
+++ soma.go
@@ -16,13 +16,36 @@ func Soma(numeros []int) int {
 	return soma
 }
 
-func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
-	quantidadeDeNumeros := len(numerosParaSomar)
-	somas = make([]int, quantidadeDeNumeros)
-
-	for i, numeros := range numerosParaSomar {
-		somas[i] = Soma(numeros)
+func SomaTudo(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		somas = append(somas, Soma(numeros))
 	}
 
-	return
+	return somas
 }
+
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+
+	return somas
+}
+
+// func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+// 	quantidadeDeNumeros := len(numerosParaSomar)
+// 	somas = make([]int, quantidadeDeNumeros)
+
+// 	for i, numeros := range numerosParaSomar {
+// 		somas[i] = Soma(numeros)
+// 	}
+
+// 	return
+// }
