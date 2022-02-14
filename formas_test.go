dda01effc2ea1ee24baffcfd3c6e2e5d145bd846
jaqueline <jaqueline.abreu@fliper.app>
2022-02-14 16:19:27 -0300

    Teste final

--- formas_test.go
+++ formas_test.go
@@ -13,18 +13,21 @@ func TestPerimetro(t *testing.T) {
 }
 func TestArea(t *testing.T) {
 	testesArea := []struct {
-		forma    Forma
-		esperado float64
+		nome    string
+		forma   Forma
+		temArea float64
 	}{
-		{Retangulo{12, 6}, 72.0},
-		{Circulo{10}, 314.1592653589793},
-		{Triangulo{12, 6}, 36.0},
+		{nome: "Retangulo", forma: Retangulo{Largura: 12, Altura: 6}, temArea: 72.0},
+		{nome: "Circulo", forma: Circulo{Raio: 10}, temArea: 314.1592653589793},
+		{nome: "Triangulo", forma: Triangulo{Base: 12, Altura: 6}, temArea: 36.0},
 	}
 
 	for _, tt := range testesArea {
-		resultado := tt.forma.Area()
-		if resultado != tt.esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, tt.esperado)
-		}
+		t.Run(tt.nome, func(t *testing.T) {
+			resultado := tt.forma.Area()
+			if resultado != tt.temArea {
+				t.Errorf("%#v resultado %.2f, esperado %.2f", tt.forma, resultado, tt.temArea)
+			}
+		})
 	}
 }
