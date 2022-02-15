290f3729af2ccb10337e1de6a6a2564f5ae96f1a
jaqueline <jaqueline.abreu@fliper.app>
2022-02-15 16:43:23 -0300

    Mensagens de erro

--- carteira_test.go
+++ carteira_test.go
@@ -5,47 +5,55 @@ import (
 )
 
 func TestCarteira(t *testing.T) {
-	confirmaSaldo := func(t *testing.T, carteira Carteira, esperado Bitcoin) {
-		t.Helper()
-		resultado := carteira.Saldo()
-
-		if resultado != esperado {
-			t.Errorf("resultado %s, esperado %s", resultado, esperado)
-		}
-	}
-
 	t.Run("Depositar", func(t *testing.T) {
 		carteira := Carteira{}
 		carteira.Depositar(Bitcoin(10))
+
 		confirmaSaldo(t, carteira, Bitcoin(10))
 	})
 
-	t.Run("Retirar", func(t *testing.T) {
-		carteira := Carteira{saldo: Bitcoin(20)}
-		carteira.Retirar(10)
+	t.Run("Retirar com saldo suficiente", func(t *testing.T) {
+		carteira := Carteira{Bitcoin(20)}
+		erro := carteira.Retirar(Bitcoin(10))
+
 		confirmaSaldo(t, carteira, Bitcoin(10))
+		confirmaErroInexistente(t, erro)
+
 	})
 
-	confirmaErro := func(t *testing.T, valor error, esperado string) {
-		t.Helper()
-		if valor == nil {
-			t.Fatal("esperava um erro, mas nenhum ocorreu")
-		}
-
-		resultado := valor.Error()
-
-		if resultado != esperado {
-			t.Errorf("resultado %s, esperado %s", resultado, esperado)
-		}
-	}
-
-	t.Run("Retirar saldo insuficiente", func(t *testing.T) {
+	t.Run("Retirar com saldo insuficiente", func(t *testing.T) {
 		saldoInicial := Bitcoin(20)
 		carteira := Carteira{saldoInicial}
 		erro := carteira.Retirar(Bitcoin(100))
 
 		confirmaSaldo(t, carteira, saldoInicial)
-		confirmaErro(t, erro, "não é possível retirar: saldo insuficiente")
+		confirmaErro(t, erro, ErroSaldoInsuficiente)
 	})
+}
 
+func confirmaSaldo(t *testing.T, carteira Carteira, esperado Bitcoin) {
+	t.Helper()
+	resultado := carteira.Saldo()
+
+	if resultado != esperado {
+		t.Errorf("resultado %s, esperado %s", resultado, esperado)
+	}
+}
+
+func confirmaErroInexistente(t *testing.T, resultado error) {
+	t.Helper()
+	if resultado != nil {
+		t.Fatal("erro inesperado recebido")
+	}
+}
+
+func confirmaErro(t *testing.T, resultado error, esperado error) {
+	t.Helper()
+	if resultado == nil {
+		t.Fatal("esperava um erro, mas nenhum ocorreu")
+	}
+
+	if resultado != esperado {
+		t.Errorf("erro resultado %s, erro esperado %s", resultado, esperado)
+	}
 }
