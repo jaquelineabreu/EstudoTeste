53d25f483bea6b1a2612d2e3cbd72407cfb9ca9c
jaqueline <jaqueline.abreu@fliper.app>
2022-02-15 16:25:55 -0300

    Carteira, ja refaturada com ponteiros e algumas mensagens de erro

--- carteira.go
+++ carteira.go
@@ -1,11 +1,40 @@
 package ponteiroserros
 
-type Carteira struct{}
+import (
+	"errors"
+	"fmt"
+)
 
-func (c Carteira) Depositar(quantidade int) {
+type Bitcoin int
 
+type Carteira struct {
+	saldo Bitcoin
 }
 
-func (c Carteira) Saldo() int {
-	return 0
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (c *Carteira) Depositar(quantidade Bitcoin) {
+	c.saldo += quantidade
+}
+
+func (c *Carteira) Saldo() Bitcoin {
+	return c.saldo
+}
+
+var ErroSaldoInsuficiente = errors.New("não é possível retirar: saldo insuficiente")
+
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+
+	if quantidade > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= quantidade
+	return nil
 }
--- carteira_test.go
+++ carteira_test.go
@@ -1,16 +1,51 @@
 package ponteiroserros
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCarteira(t *testing.T) {
-	carteira := Carteira{}
+	confirmaSaldo := func(t *testing.T, carteira Carteira, esperado Bitcoin) {
+		t.Helper()
+		resultado := carteira.Saldo()
 
-	carteira.Depositar(10)
-
-	resultado := carteira.Saldo()
-	esperado := 10
-
-	if resultado != esperado {
-		t.Errorf("resultado %d, esperado %d", resultado, esperado)
+		if resultado != esperado {
+			t.Errorf("resultado %s, esperado %s", resultado, esperado)
+		}
 	}
+
+	t.Run("Depositar", func(t *testing.T) {
+		carteira := Carteira{}
+		carteira.Depositar(Bitcoin(10))
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
+
+	t.Run("Retirar", func(t *testing.T) {
+		carteira := Carteira{saldo: Bitcoin(20)}
+		carteira.Retirar(10)
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
+
+	confirmaErro := func(t *testing.T, valor error, esperado string) {
+		t.Helper()
+		if valor == nil {
+			t.Fatal("esperava um erro, mas nenhum ocorreu")
+		}
+
+		resultado := valor.Error()
+
+		if resultado != esperado {
+			t.Errorf("resultado %s, esperado %s", resultado, esperado)
+		}
+	}
+
+	t.Run("Retirar saldo insuficiente", func(t *testing.T) {
+		saldoInicial := Bitcoin(20)
+		carteira := Carteira{saldoInicial}
+		erro := carteira.Retirar(Bitcoin(100))
+
+		confirmaSaldo(t, carteira, saldoInicial)
+		confirmaErro(t, erro, "não é possível retirar: saldo insuficiente")
+	})
+
 }
