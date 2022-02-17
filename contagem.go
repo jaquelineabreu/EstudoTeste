7d6ac039537858dda1499f32de2a44ca990979cf
jaqueline <jaqueline.abreu@fliper.app>
2022-02-17 10:41:40 -0300

    Antes de quebrar para acertar o tempo de cada Go

--- contagem.go
+++ contagem.go
@@ -4,18 +4,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const ultimaPalavra = "Go!"
 const inicioContagem = 3
 
-func Contagem(saida io.Writer) {
+type SleeperSpy struct {
+	Chamadas int
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+func (s *SleeperSpy) Sleep() {
+	s.Chamadas++
+}
+
+type SleeperPadrao struct{}
+
+func (d *SleeperPadrao) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(saida, i)
 	}
+
+	sleeper.Sleep()
 	fmt.Fprint(saida, ultimaPalavra)
 }
 
 func main() {
-	Contagem(os.Stdout)
+	sleeper := &SleeperPadrao{}
+	Contagem(os.Stdout, sleeper)
 }
