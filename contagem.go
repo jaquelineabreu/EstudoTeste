3d6b6a2553dc659513a06f91f35a0d2a1dc428cd
jaqueline <jaqueline.abreu@fliper.app>
2022-02-17 11:08:38 -0300

    Refatorando para que um teste o que está sendo impresso e o outro se certifique de que estamos pausando entre as impressões

--- contagem.go
+++ contagem.go
@@ -7,25 +7,36 @@ import (
 	"time"
 )
 
+// Sleeper te permite definir pausas
+type Sleeper interface {
+	Pausa()
+}
+
+// SleeperPadrao é uma implementação de Sleeper com um atraso pré-definido
+type SleeperPadrao struct{}
+
+// Pausa vai pausar a execução pela Duração definida
+func (d *SleeperPadrao) Pausa() {
+	time.Sleep(1 * time.Second)
+}
+
 const ultimaPalavra = "Go!"
 const inicioContagem = 3
 
-type SleeperSpy struct {
-	Chamadas int
+// Contagem imprime uma contagem de 3 para a saída com um atraso determinado por um Sleeper
+func Contagem(saida io.Writer, sleeper Sleeper) {
+	for i := inicioContagem; i > 0; i-- {
+		sleeper.Pausa()
+		fmt.Fprintln(saida, i)
+	}
+
+	sleeper.Pausa()
+	fmt.Fprint(saida, ultimaPalavra)
 }
 
-type Sleeper interface {
-	Sleep()
-}
-
-func (s *SleeperSpy) Sleep() {
-	s.Chamadas++
-}
-
-type SleeperPadrao struct{}
-
-func (d *SleeperPadrao) Sleep() {
-	time.Sleep(1 * time.Second)
+func main() {
+	sleeper := &SleeperPadrao{}
+	Contagem(os.Stdout, sleeper)
 }
 
 type SpyContagemOperacoes struct {
@@ -43,22 +54,3 @@ func (s *SpyContagemOperacoes) Write(p []byte) (n int, err error) {
 
 const escrita = "escrita"
 const pausa = "pausa"
-
-func Contagem(saida io.Writer, sleeper Sleeper) {
-	for i := inicioContagem; i > 0; i-- {
-		sleeper.Pausa()
-		fmt.Fprintln(saida, i)
-	}
-
-	for i := inicioContagem; i > 0; i-- {
-		fmt.Fprintln(saida, i)
-	}
-
-	sleeper.Pausa()
-	fmt.Fprint(saida, ultimaPalavra)
-}
-
-func main() {
-	sleeper := &SleeperPadrao{}
-	Contagem(os.Stdout, sleeper)
-}
--- contagem_test.go
+++ contagem_test.go
@@ -2,26 +2,52 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestContagem(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeperSpy := &SleeperSpy{}
 
-	Contagem(buffer, sleeperSpy)
+	t.Run("imprime 3 até Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Contagem(buffer, &SpyContagemOperacoes{})
 
-	resultado := buffer.String()
-	esperado := `3
+		resultado := buffer.String()
+		esperado := `3
 2
 1
-Vai!`
+Go!`
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-	}
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
 
-	if sleeperSpy.Chamadas != 4 {
-		t.Errorf("não houve chamadas suficientes do sleeper, esperado 4, resultado %d", sleeperSpy.Chamadas)
-	}
+	t.Run("pausa antes de cada impressão", func(t *testing.T) {
+		spyImpressoraSleep := &SpyContagemOperacoes{}
+		Contagem(spyImpressoraSleep, spyImpressoraSleep)
+
+		esperado := []string{
+			pausa,
+			escrita,
+			pausa,
+			escrita,
+			pausa,
+			escrita,
+			pausa,
+			escrita,
+		}
+
+		if !reflect.DeepEqual(esperado, spyImpressoraSleep.Chamadas) {
+			t.Errorf("esperado %v chamadas, resultado %v", esperado, spyImpressoraSleep.Chamadas)
+		}
+	})
+}
+
+type SleeperSpy struct {
+	Chamadas int
+}
+
+func (s *SleeperSpy) Pausa() {
+	s.Chamadas++
 }
