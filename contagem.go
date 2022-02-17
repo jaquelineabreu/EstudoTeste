82d6ea55a1f08a50ceae5a6f4647b2cab47a3160
jaqueline <jaqueline.abreu@fliper.app>
2022-02-17 10:44:00 -0300

    Organzando em partes

--- contagem.go
+++ contagem.go
@@ -28,13 +28,33 @@ func (d *SleeperPadrao) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type SpyContagemOperacoes struct {
+	Chamadas []string
+}
+
+func (s *SpyContagemOperacoes) Pausa() {
+	s.Chamadas = append(s.Chamadas, pausa)
+}
+
+func (s *SpyContagemOperacoes) Write(p []byte) (n int, err error) {
+	s.Chamadas = append(s.Chamadas, escrita)
+	return
+}
+
+const escrita = "escrita"
+const pausa = "pausa"
+
 func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
-		sleeper.Sleep()
+		sleeper.Pausa()
 		fmt.Fprintln(saida, i)
 	}
 
-	sleeper.Sleep()
+	for i := inicioContagem; i > 0; i-- {
+		fmt.Fprintln(saida, i)
+	}
+
+	sleeper.Pausa()
 	fmt.Fprint(saida, ultimaPalavra)
 }
 
