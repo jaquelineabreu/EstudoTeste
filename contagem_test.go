4a12f8be8fc6d478f6837f739b6939998763e6f8
jaqueline <jaqueline.abreu@fliper.app>
2022-02-17 09:53:07 -0300

    Avançando na contagem

--- contagem_test.go
+++ contagem_test.go
@@ -11,8 +11,10 @@ func TestContagem(t *testing.T) {
 	Contagem(buffer)
 
 	resultado := buffer.String()
-	esperado := "3"
-
+	esperado := `3
+				 2
+				 1
+				 Vai!`
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 	}
