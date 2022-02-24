0df181418495d040ff7bbb7464da91e6b9cbde8e
jaqueline <jaqueline.abreu@fliper.app>
2022-02-23 23:08:42 -0300

    teste funcionando perfeitamente

--- formas/formas_test.go
+++ formas/formas_test.go
@@ -1,27 +1,29 @@
 package formas
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-
-func TestArea(t *testing.T){
-	t.Run("Retângulo", func(t *testing.t)){
-		ret := Retangulo{10,12}
-		areaEsperada :=  float64(120)
+func TestArea(t *testing.T) {
+	t.Run("Retângulo", func(t *testing.T) {
+		ret := Retangulo{10, 12}
+		areaEsperada := float64(120)
 		areaRecebida := ret.Area()
 
 		if areaEsperada != areaRecebida {
-			t.Errorf("A área recebida %f é diferente da esperada %f", areaRecebida,areaEsperada)
+			t.Errorf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
 		}
 
-	}
+	})
 
-	t.Run("Círculo", func(t *testing.t)){
-  		circ := Circulo{10}
+	t.Run("Círculo", func(t *testing.T) {
+		circ := Circulo{10}
 		areaEsperada := float64(math.Pi * 100)
 		areaRecebida := circ.Area()
 
 		if areaEsperada != areaRecebida {
-			t.Errorf("A área recebida %f é diferente da esperada %f", areaRecebida,areaEsperada)
+			t.Errorf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
 		}
-	}
+	})
 }
